76c5538a5667d20c5d7381682102b192091bb82d
Lav Patel <contact.lavpatel@gmail.com>
2021-11-11 13:01:46 -0600

    jenkins .sh script dont like ""

--- grep_potential_issues.sh
+++ grep_potential_issues.sh
@@ -1,4 +1,4 @@
 set +x
-grep -i 'error' "${console_log}" >error.log
-grep 'ORA-' "${console_log}" >error_ORA.log
-grep 'SP2-' "${console_log}" >error_SP2.log
+grep -i 'error' ${console_log} >error.log
+grep 'ORA-' ${console_log} >error_ORA.log
+grep 'SP2-' ${console_log} >error_SP2.log
