7a0fc101ad209c771c7b1d37b59569995bf98bdf
Lav Patel <contact.lavpatel@gmail.com>
2021-11-11 13:06:41 -0600

    not error out if there it cant find error related words

--- grep_potential_issues.sh
+++ grep_potential_issues.sh
@@ -1,4 +1,4 @@
 set +x
-grep -i 'error' ${console_log} >error.log
-grep 'ORA-' ${console_log} >error_ORA.log
-grep 'SP2-' ${console_log} >error_SP2.log
+grep -i 'error' ${console_log} >error.log || true # true needed if there is no match found
+grep 'ORA-' ${console_log} >error_ORA.log || true # true needed if there is no match found
+grep 'SP2-' ${console_log} >error_SP2.log || true # true needed if there is no match found
